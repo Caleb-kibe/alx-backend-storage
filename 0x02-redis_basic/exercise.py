bf29cf5a8fb6ec2c064dcd6070cbec110bcb5ba7
caleb-kibe <kiplagatcaleb2@gmail.com>
2024-08-14 12:56:15 +0000

    created a get method that take a key string argument and an optional Callable argument named fn. This callable will be used to convert the data back to the desired format.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional, Any
 
 
 class Cache:
@@ -20,3 +20,30 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> None:
+        """
+        retrieve data from redis and apply an optional conversion function
+        """
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if Callable(fn):
+            return fn(value)
+
+    def get_str(self, key: str) -> Optional(str):
+        """
+        retrieve a string from redis and decode it using UTF-8
+        """
+        return data.decode('UTF-8')
+
+    def get_int(self, data: bytes) -> int:
+        """
+        retrieves an integer from redis
+        """
+        return int(data)
